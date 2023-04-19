9aa7d3f40251eae35cbc1bdb0005930c8721141b
JPHA7 <oleksandr.savchenko12@gmail.com>
2023-04-19 09:36:03 +0300

    first buttons

--- Program.cs
+++ Program.cs
@@ -1,32 +1,84 @@
 ﻿using HtmlAgilityPack;
+using Microsoft.VisualBasic;
 using Newtonsoft.Json.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bots.Types;
 
 namespace TelegBot
 {
     internal class Program
     {
         static ITelegramBotClient bot = new TelegramBotClient("6275603213:AAGcU2TVg_wTnRyvXLXTfjxMnTysGhb-e3c");
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Telegram.Bot.Types.Update update, CancellationToken cancellationToken)
         {
             // Некоторые действия
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
                 var message = update.Message;
-                if (message.Text.ToLower() == "/start")
+                if (message.Text.ToLower() == "/start" || message.Text.ToLower() == "повернутись в меню")
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, GetRequest("http://213.108.47.12:3050/cgi-bin/timetable.cgi?n=700&group=2491"));
+                    await botClient.SendTextMessageAsync(message.Chat, "Вітаємо!");
+                    var buttons = new KeyboardButton[][]
+                    {
+                    new KeyboardButton[]
+                        {
+                            new KeyboardButton("Показати розклад")
+                        },
+
+                        new KeyboardButton[]
+                        {
+                            new KeyboardButton("Показати свій розклад")
+                        },
+
+                        new KeyboardButton[]
+                        {
+                            new KeyboardButton("Налаштування")
+                        }
+                    };
+                    var rkm = new ReplyKeyboardMarkup(buttons);
+
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Оберіть дію", replyMarkup: rkm);
                     return;
                 }
-                await botClient.SendTextMessageAsync(message.Chat, "Ещкере");
+                else if (message.Text.ToLower() == "показати розклад")
+                {
+                    var buttons = new KeyboardButton[][]
+                    {
+                    new KeyboardButton[]
+                        {
+                            new KeyboardButton("Обрати групу")
+                        },
+
+                        new KeyboardButton[]
+                        {
+                            new KeyboardButton("Обрати викладача")
+                        },
+
+                        new KeyboardButton[]
+                        {
+                            new KeyboardButton("Повернутись в меню")
+                        }
+                    };
+                    var rkm = new ReplyKeyboardMarkup(buttons);
+
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Оберіть дію", replyMarkup: rkm);
+                    return;
+                }
+                await botClient.SendTextMessageAsync(message.Chat, "Something went wrong :)");
             }
+
+
         }
 
         public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
@@ -47,18 +99,22 @@ namespace TelegBot
                 AllowedUpdates = { }, // receive all update types
             };
             bot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken);
+
+            
             Console.ReadLine();
         }
+
         static string GetRequest(string url)
         {
             Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             HtmlWeb webGet = new HtmlWeb();
             webGet.OverrideEncoding = Encoding.GetEncoding(1251);
             HtmlDocument doc = webGet.Load(url);
-            //Console.WriteLine(doc.DocumentNode.OuterHtml);
-            //Console.ReadLine();
             string res = doc.DocumentNode.InnerText;
             return res;
+            //Console.WriteLine(doc.DocumentNode.OuterHtml);
+            //Console.ReadLine();
+
             //Console.WriteLine(res);
             //HtmlNode myNode = doc.DocumentNode.SelectSingleNode("div//[@class='row']");
             //Console.WriteLine(myNode.InnerText);
